9aeaa1b286c0bc5828f0c6b2d540192af3c20f1b
Jimmy Yen <zeng.tw@gmail.com>
2019-09-22 13:17:54 +0800

    nothing

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the cawa0505/laravel-thl-pinyin.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use THL\Pinyin;
 
 if (!function_exists('pinyin')) {
